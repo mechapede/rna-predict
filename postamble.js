3904ea0d86e0d97b53f8b03ca7c25b1f887105d9
Mechapede <26446679+mechapede@users.noreply.github.com>
2020-08-12 19:49:06 -0700

    Fix type in code

--- postamble.js
+++ postamble.js
@@ -2,5 +2,5 @@
 export var find_minimums = Module.cwrap('find_minimums', 'number', ["string"]);
 export var get_minimum = Module.cwrap('get_minimum', "string", ["number"]);
 export var number_minimums = Module.cwrap('number_minimums', "number");
-export var minimum_score = Module.cwrap('mimimum_score', 'number');
+export var minimum_score = Module.cwrap('minimum_score', 'number');
 export var set_weights = Module.cwrap('set_weights', null, ["number", "number", "number", "number", "number"]);
